3ba230b691f0c44221d4f0188c84310c212b52c4
Quan Tran <quan.tran@fullertonhealth.com>
2022-03-21 10:53:45 +0700

    add bgd

--- util/field.js
+++ util/field.js
@@ -1,400 +1,328 @@
-export const fieldExcel = [
+const field = [
     {
-        id: 1,
+        inputField: "BGĐ",
+        outputField: "Ban Giám Đốc"
+    },
+    {
         inputField: "3B1",
         outputField: "3b1"
     },
     {
-        id: 2,
         inputField: "3B3",
         outputField: "3B3"
     },
     {
-        id: 3,
         inputField: "4B1",
         outputField: "4B1"
     },
     {
-        id: 4,
         inputField: "4B3",
         outputField: "4B3"
     },
     {
-        id: 5,
         inputField: "5B1",
         outputField: "5B1"
     },
     {
-        id: 6,
         inputField: "5B3",
         outputField: "5B3"
     },
     {
-        id: 7,
         inputField: "6B1",
         outputField: "6B1"
     },
     {
-        id: 8,
         inputField: "6B3",
         outputField: "6B3"
     },
     {
-        id: 9,
         inputField: "7B1",
         outputField: "7B1"
     },
     {
-        id: 10,
         inputField: "7B3",
         outputField: "7B3 (nội tim mạch - rối loạn nhịp)"
     },
     {
-        id: 11,
         inputField: "8B1",
         outputField: "8B1"
     },
     {
-        id: 12,
         inputField: "8B3",
         outputField: "8B3"
     },
     {
-        id: 13,
         inputField: "9B1",
         outputField: "9B1"
     },
     {
-        id: 14,
         inputField: "9B3",
         outputField: "9B3"
     },
     {
-        id: 15,
         inputField: "10B1",
         outputField: "10B1"
     },
     {
-        id: 16,
         inputField: "10B3",
         outputField: "10B3"
     },
     {
-        id: 17,
         inputField: "KKB",
         outputField: "Khoa khám bệnh - Khu A"
     },
     {
-        id: 18,
         inputField: "KKB PK-D",
         outputField: "Khoa khám bệnh - PK D"
     },
     {
-        id: 19,
         inputField: "Khám bệnh II",
         outputField: "Khám bệnh II"
     },
     {
-        id: 20,
         inputField: "CSSKTYC",
         outputField: "Chăm sóc sức khỏe theo yêu cầu"
     },
     {
-        id: 21,
         inputField: "Khám xuất cảnh",
         outputField: "Khám Xuất cảnh"
     },
     {
-        id: 22,
         inputField: "Siêu âm",
         outputField: "Siêu âm"
     },
     {
-        id: 23,
         inputField: "Nội soi",
         outputField: "Nội soi"
     },
     {
-        id: 24,
         inputField: "HSPTT",
         outputField: "Hồi sức Phẫu thuật tim"
     },
     {
-        id: 25,
         inputField: "HSPTTTE",
         outputField: "Hồi sức Phẫu thuật tim trẻ em"
     },
     {
-        id: 26,
         inputField: "GMPTT",
         outputField: "Gây mê phẫu thuật tim"
     },
     {
-        id: 27,
         inputField: "HSNTK",
         outputField: "Hồi sức Ngoại Thần kinh"
     },
     {
-        id: 28,
         inputField: "ICU-B",
         outputField: "Hồi sức cấp cứu khu B"
     },
     {
-        id: 29,
         inputField: "PT-GMHS",
         outputField: "Phẫu thuât - Gây mê hồi sức"
     },
     {
-        id: 30,
         inputField: ["TLTT-KSNK", "KSNK"],
         outputField: "Kiểm soát nhiễm khuẩn"
     },
     {
-        id: 31,
         inputField: ["Sinh hóa", "Sinh hóa D"],
         outputField: "Sinh hóa"
     },
     {
-        id: 32,
         inputField: "Vi sinh",
         outputField: "Vi sinh"
     },
     {
-        id: 33,
         inputField: "Giải phẫu bệnh",
         outputField: "Giải phẫu bệnh"
     },
     {
-        id: 34,
         inputField: "TTTM",
         outputField: "Trung tâm truyền máu"
     },
     {
-        id: 35,
         inputField: "Huyết học CLS",
         outputField: "Huyết học CLS"
     },
     {
-        id: 36,
         inputField: "Huyết học LS",
         outputField: "Huyết học LS"
     },
     {
-        id: 37,
         inputField: "BNĐ",
         outputField: "Bệnh nhiệt đới"
     },
     {
-        id: 38,
         inputField: "Viêm gan",
         outputField: "Viêm gan"
     },
     {
-        id: 39,
         inputField: "YHHN",
         outputField: "Y học hạt nhân"
     },
     {
-        id: 40,
         inputField: "Cấp cứu",
         outputField: "Cấp cứu"
     },
     {
-        id: 41,
         inputField: "CĐHA",
         outputField: "Chẩn đoán hình ảnh"
     }, {
-        id: 42,
         inputField: "HECI",
         outputField: "HECI"
     },
     {
-        id: 43,
         inputField: "CTSN",
         outputField: "Chấn thương sọ não"
     },
     {
-        id: 44,
         inputField: "Hóa trị",
         outputField: "Hóa trị"
     },
     {
-        id: 45,
         inputField: "Xạ trị",
         outputField: "Xạ trị"
     },
     {
-        id: 46,
         inputField: "Giảm nhẹ",
         outputField: "Giảm nhẹ"
     },
     {
-        id: 47,
         inputField: "Tuyến vú",
         outputField: "Tuyến vú"
     },
     {
-        id: 48,
         inputField: "ICU-D",
         outputField: "Hồi sức tích cực khu D"
     },
     {
-        id: 49,
         inputField: "Gamma-Knife",
         outputField: "Gamma-Knife"
     },
     {
-        id: 50,
         inputField: "Nội khớp",
         outputField: "Nội Cơ xương khớp"
     },
     {
-        id: 51,
         inputField: "Nội thận",
         outputField: "Nội Thận"
     },
     {
-        id: 52,
         inputField: "Nội tiết",
         outputField: "Nội tiết"
     },
     {
-        id: 53,
         inputField: "PHCN",
         outputField: "Phục hồi chức năng"
     },
     {
-        id: 54,
         inputField: "Phỏng",
         outputField: "Phỏng"
     },
     {
-        id: 55,
         inputField: "TMCT",
         outputField: "Tim mạch can thiệp"
     },
     {
-        id: 56,
         inputField: "PTMM",
         outputField: "Phẫu thuật mạch máu"
     },
     {
-        id: 57,
         inputField: "Thẩm mỹ",
         outputField: "Thẩm mỹ"
     },
     {
-        id: 58,
         inputField: "T6YC",
         outputField: "Trại 6 yêu cầu"
     },
     {
-        id: 59,
         inputField: "TNT",
         outputField: "Thận nhân tạo"
     },
     {
-        id: 60,
         inputField: "U gan",
         outputField: "U gan"
     },
     {
-        id: 61,
         inputField: "Phòng ĐD",
         outputField: "Phòng Điều dưỡng"
     },
     {
-        id: 62,
         inputField: "Dinh dưỡng",
         outputField: "Dinh dưỡng"
     },
     {
-        id: 63,
         inputField: "Dược",
         outputField: "Dược"
     },
     {
-        id: 64,
         inputField: "Bảo vệ",
         outputField: "Bảo vệ"
     },
     {
-        id: 65,
         inputField: "CNTT",
         outputField: "Công nghệ thông tin"
     },
     {
-        id: 66,
         inputField: null,
         outputField: "Tổ chức cán bộ"
     },
     {
-        id: 67,
         inputField: "KHTH",
         outputField: "Kế hoạch tổng hợp"
     },
     {
-        id: 68,
         inputField: "Quản trị",
         outputField: "Quản trị"
     },
     {
-        id: 69,
         inputField: "Công xa",
         outputField: "Công xa"
     },
     {
-        id: 70,
         inputField: " Tổ chức hành chính",
         outputField: "Phòng Tổ chức Hành chính"
     },
     {
-        id: 71,
         inputField: "QLDA",
         outputField: "Quản lý dự án"
     }, {
-        id: 72,
         inputField: 'QLDA-CR2',
         outputField: "Quản lý dự án - Chợ Rẫy 2"
     },
     {
-        id: 73,
         inputField: "TCKT",
         outputField: "Tài chính kế toán"
     },
     {
-        id: 74,
         inputField: "Công đoàn",
         outputField: "Công đoàn"
     },
     {
-        id: 75,
         inputField: 'CTXH',
         outputField: "Công tác xã hội"
     },
     {
-        id: 76,
         inputField: 'ĐV đấu thầu',
         outputField: "Đơn vị đấu thầu"
     },
     {
-        id: 77,
         inputField: "TTĐT-CĐT",
         outputField: "Trung tâm đào tạo - Chỉ đạo tuyến"
     },
     {
-        id: 78,
         inputField: 'QLCL',
         outputField: "Quản lý chất lượng"
     },
     {
-        id: 79,
         inputField: 'TTBYT',
         outputField: "Trang thiết bị y tế"
     },
     {
-        id: 80,
         inputField: "ĐV ATVS lao động - Bức xạ",
         outputField: "Đơn vị An toàn vệ sinh lao động - Bức xạ"
     },
 ]
+
+export const fieldExcel = field.map((item, key) => {
+    return { ...item, id: key + 1 }
+})
